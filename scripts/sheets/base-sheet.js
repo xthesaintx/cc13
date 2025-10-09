20b32b92b2b0a52c087cfd5e66a0323c152671ed
Stephen Garrett <42524322+xthesaintx@users.noreply.github.com>
2025-10-09 17:48:04 +1300

    0.8.6

--- scripts/sheets/base-sheet.js
+++ scripts/sheets/base-sheet.js
@@ -797,7 +797,6 @@ _onToggleTags(event) {
     nameElement.textContent = this.document.name;
     input.replaceWith(nameElement);
     nameElement.addEventListener("click", this._onNameEdit.bind(this));
-    if(this.document.getFlag("campaign-codex", "data")?.tagMode) {game.campaignCodex.updateTagInCache(this.document);}
   }
 
   async _onNameKeypress(event) {
